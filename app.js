432353a1b9e962457a4c1db4af6fa1fd40a9ae1c
Shattik <1905008@ugrad.cse.buet.ac.bd>
2024-01-18 00:36:10 +0600

    apiWritten

--- app.js
+++ app.js
@@ -1,17 +1,54 @@
 const supabase =  require("./db.js");
 const express = require('express'); 
+const cors = require('cors');
 require('dotenv').config();
 const app = express(); 
 const PORT = process.env.port;
-console.log(PORT);
 
-app.listen(PORT, async (error) =>{ 
-    if(!error){
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT); 
-        const data = await supabase.any('SELECT * FROM "Division"');
-        console.log(data);
+app.use(express.json());
+app.use(cors({origin: '*'}));
+
+app.get('/division', async (req, res) => {
+    try{
+        const data = await supabase.any('SELECT id, name FROM "Division"');
+        res.json(data);
     }
-    else
-        console.log("Error occurred, server can't start", error); 
-    } 
-); 
+    catch(error){
+        console.log(error);
+    }
+});
+
+app.post('/district', async (req, res) => {
+    try{
+        const division = req.body['division']; 
+        const data = await supabase.any('SELECT id, name FROM "District" WHERE "divisionId" = $1', [division]);
+        res.json(data);
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+app.post('/upazilla', async (req, res) => {
+    try{
+        const district = req.body['district']; 
+        const data = await supabase.any('SELECT id, name FROM "Upazilla" WHERE "districtId" = $1', [district]);
+        res.json(data);
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+app.post('/union', async (req, res) => {
+    try{
+        const upazilla = req.body['upazilla']; 
+        const data = await supabase.any('SELECT id, name FROM "UnionParishad" WHERE "upazillaId" = $1', [upazilla]);
+        res.json(data);
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+app.listen(PORT, () => {});
