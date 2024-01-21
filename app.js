420d581a21f8e054e1e7f022fbd121510dfbeb13
tahmid-404-20 <1905002@ugrad.cse.buet.ac.bd>
2024-01-21 17:54:25 +0600

    won't push, added service registry

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const supabase =  require("./db.js");
 const express = require('express'); 
 const cors = require('cors');
 require('dotenv').config();
+const axios = require("axios");
 const app = express(); 
 const PORT = process.env.port;
 
@@ -51,6 +52,43 @@ app.post('/union', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Pushti-LocationMS is running on port ${PORT}`);
-});
+app.listen(PORT, async () => {
+    // console.log(`Pushti-AuthenticationMS is running on port ${PORT}`);
+    console.log(`Pushti-LocationMs listening on port ${PORT}`);
+    try {
+      let serviceRegisterUrl = String(process.env.serviceRegistryUrl) + "/register";
+  
+      await axios.post(serviceRegisterUrl, {
+        name: process.env.selfName,
+        url: process.env.selfUrl,
+      });
+      console.log("Service registered successfully");
+    } catch (error) {
+      console.error("Failed to register service:", error);
+      // turn off server if service registration fails
+      process.exit(1);
+    }
+  });
+  
+  // Function to de-register the service
+  const deregisterService = async () => {
+    try {
+      let serviceRegisterUrl =
+        String(process.env.serviceRegistryUrl) + "/deregister";
+      await axios.post(serviceRegisterUrl, { name: process.env.selfName });
+      console.log("Service de-registered successfully");
+    } catch (error) {
+      console.error("Failed to de-register service:", error);
+      process.exit(1);
+    }
+  };
+  
+  const gracefulShutdown = async () => {
+      await deregisterService();
+      process.exit(0);
+  };
+  
+  // Listen for termination and interrupt signals
+  process.on('SIGTERM', gracefulShutdown); // For termination signal
+  process.on('SIGINT', gracefulShutdown); // For interrupt signal
+  process.on('uncaughtException', gracefulShutdown); // For uncaught exceptions
